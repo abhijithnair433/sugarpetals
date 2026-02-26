a90bc3d5e28dfe10c304cede3587ac4d8e009c78
abhijithnair433 <abhijithnair433@gmail.com>
2026-02-26 21:19:24 +0530

    added serializers for accounts carts orders products

---
+++ sugarpetals/orders/serializers.py
@@ -0,0 +1,18 @@
+from rest_framework import serializers
+from .models import Order, OrderItem
+from products.serializers import ProductSerializer
+
+class OrderItemSerializer(serializers.ModelSerializer):
+    product = ProductSerializer(read_only=True)
+
+    class Meta:
+        model = OrderItem
+        fields = ['id', 'product', 'quantity']
+
+class OrderSerializer(serializers.ModelSerializer):
+    items = OrderItemSerializer(many=True, read_only=True)
+
+    class Meta:
+        model = Order
+        fields = ['id', 'status', 'created_at', 'items']
+        read_only_fields = ['status', 'created_at']
